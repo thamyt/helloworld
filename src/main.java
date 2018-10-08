746acd866d2f55dcb71f01a05ddbcc67593832b5
ASUS-S510UN\YT <thamyt75@gmail.com>
2018-10-08 11:26:33 +0800

    Test

--- src/main.java
+++ src/main.java
@@ -1,6 +1,10 @@
 public class AppMain {
 	static public void main(String[] args) {
 		system.out.println("Hello 123");
+		system.out.println("Hello 123");
+		system.out.println("Hello 123");
+		system.out.println("Hello 123");
+		system.out.println("Hello 123");
 		system.out.println("Have a nice day.");
 	}
 }
