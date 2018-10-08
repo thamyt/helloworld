37013ea85e66ee9e74b1a00f414c7c97e58365a2
ASUS-S510UN\YT <thamyt75@gmail.com>
2018-10-08 11:08:59 +0800

    modified java program

--- src/main.java
+++ src/main.java
@@ -1,5 +1,6 @@
 public class AppMain {
 	static public void main(String[] args) {
-		system.out.println("Hello World");
+		system.out.println("Hello 123");
+		system.out.println("Have a nice day.");
 	}
 }
